1c0c81566d1ee4ee62f7bd664648bd864ea487fe
Atal Kumar <atalkhandewal@gmail.com>
2025-06-21 14:05:26 +0530

    added 1.2 (1.0)

---
+++ 01_Getting_Started/1_1/exc_1_2.cpp
@@ -0,0 +1,14 @@
+/*
+ *Exercise 1.2: Change the program to return -1. A return value of -1 is often treated 
+  as an indicator that the program failed. Recompile and re-run your program to see how 
+  your system treats a failure indicator from main.
+*/
+
+// program
+
+#include <iostream>
+
+int main()
+{
+    return -1;
+}
