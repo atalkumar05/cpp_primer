6033064fe71d8a6bfbefde09d65cd2aecb2cbdbf
Atal Kumar <atalkhandewal@gmail.com>
2025-06-21 16:17:55 +0530

    added 1_7

---
+++ 01_Getting_Started/1_3/1_7.cpp
@@ -0,0 +1,55 @@
+// Exercise 1.7: Compile a program that has incorrectly nested comments.
+
+#include <iostream> 
+
+// -> Error due to nesting of pair comments
+/*
+* /*
+    Simple main function:
+*   Read two numbers and write their sum
+  */
+*/      
+
+int main()
+{
+// prompt user to enter two numbers
+std::cout << "Enter two numbers:" << std::endl; 
+int v1 = 0, v2 = 0; // variables to hold the input we read 
+std::cin >> v1 >> v2; // read input
+std::cout << "The sum of " << v1 << " and " << v2
+<< " is " << v1 + v2 << std::endl;
+return 0;
+}
+
+/*
+ Output: (-> Compiler shows an error)
+ error: expected unqualified-id before '/' token ...
+*/
+//-----------------------------------------------------------------------------
+
+// Can be fixed as
+
+/*
+ #include <iostream> 
+
+  
+ // Simple main function:
+ // Read two numbers and write their sum
+    
+      
+
+ int main()
+ {
+ // prompt user to enter two numbers
+ std::cout << "Enter two numbers:" << std::endl; 
+ int v1 = 0, v2 = 0; // variables to hold the input we read 
+ std::cin >> v1 >> v2; // read input
+ std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+ return 0;
+ }
+
+ Output: Enter two numbers:
+ 2
+ 2
+ The sum of 2 and 2 is 4
+*/
