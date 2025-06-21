2b51f909731d3e0238c07749eede6999f975d1ec
Atal Kumar <atalkhandewal@gmail.com>
2025-06-21 13:52:27 +0530

    added program 1.1 (1.0)

---
+++ 01_Getting_Started/1_1/exc_1_1.cpp
@@ -0,0 +1,23 @@
+/*
+ *Exercise 1.1: Review the documentation for your compiler and determine what ﬁle 
+  naming convention it uses.
+ *Compile and run the main program from page 2.
+*/
+
+/*
+ *Answer: I'm using G++ with version 11.2.0 on Windows.
+          The source files are named with the ".cpp" extension, and i can specify the output name using "-o"
+          flag (eg., hello.exe).
+          Otherwise the default output is a.exe.alignas
+*/
+
+// main program
+
+#include <iostream>
+
+int main()
+{
+   return 0;
+}
+
+
